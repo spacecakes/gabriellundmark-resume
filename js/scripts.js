c2f51c6ce9fc0833a26b9dec8f705846c91ea628
Gabriel <g.lundmark@gmail.com>
2018-03-13 13:13:59 +0100

    grid area names

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,26 @@
-// load letter from txt file
-function loadLetter() {
-    const xhr = new XMLHttpRequest();
-    const letter = document.getElementById('letter');
-    xhr.open('GET', 'letter.txt', true);
+// Load letter from txt file
 
-    xhr.onload = function () {
-        if (this.status === 200)
-            letter.innerHTML = '<p>' + this.responseText.replace(/(?:\r\n|\r|\n)/g, '</p><p>') + '</p>';
-        else
-            letter.innerHTML = '<p>(Ladda upp letter.txt för att generera personligt brev)</p>';
-    };
-    xhr.send();
+// function loadLetter() {
+//     const xhr = new XMLHttpRequest();
+//     const letter = document.getElementById('letter');
+//     xhr.open('GET', 'letter.txt', true);
+
+//     xhr.onload = function () {
+//         if (this.status === 200)
+//             letter.innerHTML = '<p>' + this.responseText.replace(/(?:\r\n|\r|\n)/g, '</p><p>') + '</p>';
+//         else
+//             letter.innerHTML = '<p>(>Ladda upp letter.txt för att generera personligt brev)</p>';
+//     };
+//     xhr.send();
+// }
+
+// Rewritten using fetch
+let letter = document.getElementById('letter');
+function loadLetter() {
+    fetch('letter.txt')
+        .then(response => response.text())
+        .then(data => letter.innerHTML = '<p>' + data.replace(/(?:\r\n|\r|\n)/g, '</p><p>') + '</p>');
 }
 
 loadLetter();
+
