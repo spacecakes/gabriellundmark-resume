b8d8f0a7214a7e20e343b853e66fe7e944c85d50
Gabriel <g.lundmark@gmail.com>
2018-03-19 11:13:37 +0100

    animate on scroll

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,4 @@
 // Load letter from txt file
-
-// function loadLetter() {
-//     const xhr = new XMLHttpRequest();
-//     const letter = document.getElementById('letter');
-//     xhr.open('GET', 'letter.txt', true);
-
-//     xhr.onload = function () {
-//         if (this.status === 200)
-//             letter.innerHTML = '<p>' + this.responseText.replace(/(?:\r\n|\r|\n)/g, '</p><p>') + '</p>';
-//         else
-//             letter.innerHTML = '<p>(>Ladda upp letter.txt för att generera personligt brev)</p>';
-//     };
-//     xhr.send();
-// }
-
-// Rewritten using fetch
 let letter = document.getElementById('letter');
 function loadLetter() {
     fetch('letter.txt')
@@ -24,4 +8,3 @@ function loadLetter() {
 }
 
 loadLetter();
-
