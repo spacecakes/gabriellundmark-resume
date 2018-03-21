9d2efe3fdefec7109c6b436beaf359da1532cbec
Gabriel <g.lundmark@gmail.com>
2018-03-21 09:10:02 +0100

    seems to be working

---
+++ js/letter.js
@@ -0,0 +1,22 @@
+// Load letter from txt file
+let letter = document.getElementById('letter');
+
+// Format line breaks as paragraphs
+function formatLetter(txt) {
+    return '<p>' + txt.replace(/(?:\r\n|\r|\n)/g, '</p><p>') + '</p>';
+}
+
+// Upload text file
+function uploadLetter(evt) {
+    const file = evt.target.files[0];
+
+    if (file) {
+        const reader = new FileReader();
+        reader.onload = e => letter.innerHTML = formatLetter(e.target.result);
+        reader.readAsText(file);
+    } else
+        letter.innerHTML = '<p>Failed to load file</p>';
+}
+
+// Listen for file upload 
+document.getElementById('letter-upload').addEventListener('change', uploadLetter, false); 
